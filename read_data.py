01c936e4cc535b9d49baedb792055a4cd1bb6a14
brendanjmeade <brendanjmeade@users.noreply.github.com>
2018-08-07 23:34:20 -0400

    A bit of formatting.  Black formatter not working at home :(

--- read_data.py
+++ read_data.py
@@ -1,35 +1,36 @@
 import h5py
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 LINE_WIDTH = 0.5
 
-# Read Zach Ross data
-data = h5py.File('scsn_p_2000_2017_6sec_0.5r_pick_train.hdf5')
-for key in data.keys():
-    print(key)
+
+def plot_time_series(data, idx):
+    ''' Extract and plot a single time series '''
+    time_series = data['X'].value[idx, :]
+    time_vec = np.linspace(-3.0, 3.0, time_series.size)
+    label = data['sncls'].value[idx].decode('UTF-8')
+    magnitude = data['mag'].value[idx]
+
+    plt.figure(figsize=(12, 6))
+    plt.fill([0, 3, 3, 0], [-1, -1, 1, 1], c=[0.85, 0.85, 0.85])
+    plt.plot(time_vec, time_series, '-b', linewidth=LINE_WIDTH)
+    plt.xlabel(r'$\Delta \; \mathrm{time} \; \mathrm{(s)}$')
+    plt.ylabel('normalized acceleration')
+    plt.title(label + ', M=' + str(magnitude))
+    plt.xlim(-3, 3)
+    plt.ylim(-1.0, 1.0)
+    plt.xticks(np.arange(-3, 3.5, 0.5))
+    plt.yticks([-1, 0, 1])
+    plt.show(block=False)
+    plt.savefig(label + '.pdf')
 
 
-# Extract a time series
-idx = 1
-time_series = data['X'].value[idx, :]
-time_vec = np.linspace(-3.0, 3.0, time_series.size)
-label = data['sncls'].value[idx].decode('UTF-8')
-magnitude = data['mag'].value[idx]
+def main():
+    ''' Read and plot Zach Ross data '''
+    data = h5py.File('scsn_p_2000_2017_6sec_0.5r_pick_train.hdf5')
+    plot_time_series(data, 1)
 
 
-# Plot
-plt.close('all')
-plt.figure(figsize=(12, 6))
-plt.fill([0, 3, 3, 0], [-1, -1, 1, 1], c=[0.85, 0.85, 0.85])
-plt.plot(time_vec, time_series, '-b', linewidth=LINE_WIDTH)
-# plt.plot(time_vec[300:-1], time_series[300:-1], '-b', linewidth=LINE_WIDTH)
-plt.xlabel(r'$\Delta \; \mathrm{time} \; \mathrm{(s)}$')
-plt.ylabel('normalized acceleration')
-plt.title(label + ', M=' + str(magnitude))
-plt.xlim(-3, 3)
-plt.ylim(-1.0, 1.0)
-plt.xticks(np.arange(-3, 3.5, 0.5))
-plt.yticks([-1, 0, 1])
-plt.show(block=False)
-plt.savefig(label + '.pdf')
+if __name__ == "__main__":
+    main()
